218393f91d952d803aa8ae8ef5fd66d9455bade6
Crowdbotics <team@crowdbotics.com>
2023-07-04 09:57:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xtra-ar-app-41897",
+  url: "https://xtra-ar-app-41897.botics.co",
+  api: "https://xtra-ar-app-41897.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
